9d1b2dc6615bdfbe802cebb32ec91fe05f3ef7f2
Gerardo Toledo <jgerardo.toledor@gmail.com>
2026-05-15 10:11:57 -0600

    [UPDATE] Bisuinesses route added

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\BusinessController;
 use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\UserController;
 
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/roles', [RoleController::class, 'index']);
+Route::get('/businesses', [BusinessController::class, 'index']);
